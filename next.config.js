b49fec2e16366e066f818ef55784662dd69ced1d
khalilos34 <khalil.naoueji@gmail.com>
2023-04-06 21:40:39 +0100

    change next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ["as2.ftcdn.net", "lh3.googleusercontent.com"],
